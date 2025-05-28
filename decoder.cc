fd482f16771a8458710aa9c4da52cd82aad4e682
Ritish Pentapalli <ritishpentapalli4@gmail.com>
2025-05-27 19:37:24 -0600

    Minor restructuring

--- decoder.cc
+++ decoder.cc
@@ -1,30 +1,41 @@
 #include "decoder.h"
 #include <iostream>
 #include <fstream>
+#include <utility>
 
 using namespace std;
 
+pair<byte_t, byte_t> getNextTwoBytes(ifstream &file) {
+
+  byte_t previous = file.get();
+  byte_t current = file.get();
+
+  return make_pair(previous, current);
+}
+
 int main(int argc, char* argv[]) {
   
-  if (argc != 2) {
+  if (argc != 2) { // Note: only decodes one image
     cout << "Invalid Usage!" << "\n";
     cout << "Correct Usage: decoder.out <path-to-jpeg-file>";
     return 1;
   }
 
+  // TODO: Maybe reject files not ending with .jpg?
+
   const string fileName = argv[1];
   
   ifstream infile;
   infile.open(fileName, ios::binary | ios::in);
+
   cout << "Decoding jpeg file: "<< fileName << "\n";
 
-  byte_t previous = infile.get();
-  byte_t current = infile.get();
-
+  auto [previous, current] = getNextTwoBytes(infile);
 
   if (previous == PREFIX && current == SOI) {
-    cout << "Works so far" << "\n";
+    cout << "Error - Not a JPEG file (does not begin with 0xFFD8)" << "\n";
+    return 1;
   } 
-	return 0;	
 
+	return 0;	
 }
